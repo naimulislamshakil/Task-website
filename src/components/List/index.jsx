d0ffd8d2d98a2c752142aab8fe108b9873303871
Naimul Islam <ms0413354@gmail.com>
2023-04-27 15:37:29 +0600

    show all data in a table

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,4 +1,14 @@
-import { Box, Typography } from '@mui/material';
+import {
+	Box,
+	Button,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+} from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllDataAction } from '../../Redux/Action';
@@ -12,10 +22,37 @@ const List = () => {
 		dispatch(getAllDataAction());
 	}, [dispatch]);
 
-	console.log(message);
+	console.log(message.data);
 	return (
 		<Box>
-			<Typography variant="h2">hgfadytsd6t</Typography>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell align="center">NAME</TableCell>
+							<TableCell align="center">SELECTOR</TableCell>
+							<TableCell align="center">EDIT BUTTON</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{message.data &&
+							message.data.map((row) => (
+								<TableRow
+									key={row.id}
+									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+									<TableCell component="th" scope="row" align="center">
+										{row.name}
+									</TableCell>
+									<TableCell align="center">{row.selector}</TableCell>
+									<TableCell align="center">
+										<Button variant="outlined">EDIT</Button>
+									</TableCell>
+								</TableRow>
+							))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</Box>
 	);
 };
