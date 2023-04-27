107fed2361798fa3e12923a2c712c68a0cb6e58b
Naimul Islam <ms0413354@gmail.com>
2023-04-27 16:08:41 +0600

    get single data by id

--- server/Routes/V1/selector.route.js
+++ server/Routes/V1/selector.route.js
@@ -1,6 +1,9 @@
 const routers = require('express').Router();
 const SelectorCollactio = require('../../Collaction/selecttors.collaction');
 
+routers
+	.route('/getSelector/:id')
+	.get(SelectorCollactio.getSelectorByIdColiction);
 routers.route('/getSelector').get(SelectorCollactio.getSelectorColiction);
 routers.route('/selector').post(SelectorCollactio.createSelectorColiction);
 
--- server/Services/selector.service.js
+++ server/Services/selector.service.js
@@ -9,3 +9,7 @@ exports.getSelectorService = async () => {
 	const result = await INPUT_SCHEMA.find();
 	return result;
 };
+exports.getSelectorByIdService = async (id) => {
+	const result = await INPUT_SCHEMA.find({ _id: id });
+	return result;
+};
