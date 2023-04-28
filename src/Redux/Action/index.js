b867ca60c9aec8699c538c70b353ce970f34c76c
Naimul Islam <ms0413354@gmail.com>
2023-04-28 12:21:58 +0600

    project task complete

--- src/Redux/Action/index.js
+++ src/Redux/Action/index.js
@@ -23,7 +23,7 @@ export const getDataAction = () => async (dispatch) => {
 			type: DATA_LOADING,
 		});
 
-		const res = await axios.get('http://localhost:5001/data');
+		const res = await axios.get('https://test-backend-2-kappa.vercel.app/data');
 
 		dispatch({
 			type: DATA_SUCCESS,
